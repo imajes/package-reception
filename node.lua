02c889978f6bee09cd500f6da5e603ee9f0e1737
James Cox <james@imaj.es>
2018-10-11 14:24:56 -0400

    more layout tweaks for da box

--- node.lua
+++ node.lua
@@ -974,7 +974,7 @@ local function Playlist()
             fn = Flat{
                 fade_time = 0,
                 color = '#000000',
-                opacity = 0.6,
+                opacity = 0.75,
             },
             coord = tile_center_overlay,
         }
